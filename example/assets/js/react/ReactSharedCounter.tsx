2a063798aa92add5ec045f627d84c06df0f3884a
Ethan Dang <tiencuong.dang@gmail.com>
2024-01-17 02:51:27 +0700

    add live_render section in iframe

--- example/assets/js/react/ReactSharedCounter.tsx
+++ example/assets/js/react/ReactSharedCounter.tsx
@@ -13,7 +13,10 @@ export const ReactSharedCounter = ({
     [data.counterData?.counter],
   );
   return (
-    <div className="w-full flex flex-col gap-3 items-stretch p-4 rounded-lg border-dashed border-zinc-500 border-2">
+    <div className="w-full flex flex-col gap-3 items-stretch p-4 rounded-lg border-zinc-500 border overflow-hidden relative">
+      <div className="absolute top-0 left-0 px-1.5 py-0.5 text-white bg-zinc-500 font-semibold text-xs rounded-br">
+        REACT ISLAND
+      </div>
       <div className="flex flex-row gap-3 items-center justify-between">
         <div>Global State: {data.counterData.counter}</div>
         <div>Local State: {counter}</div>
--- example/lib/example_web/live/embedded_live.ex
+++ example/lib/example_web/live/embedded_live.ex
@@ -1,30 +1,16 @@
-defmodule ExampleWeb.IFrameIslandsLive do
+defmodule ExampleWeb.EmbeddedLive do
   use ExampleWeb, :live_view_embedded
   import PhoenixIslands
 
   def render(assigns) do
     ~H"""
-    <div class="p-4">
-      <div class="mx-auto max-w-xl lg:mx-0 flex flex-col gap-4">
-        <p class="text-[1.2rem] font-semibold leading-10 tracking-tighter text-zinc-900">
-          Live iframe inside dead-view: React Island
+    <div class="p-3">
+      <div class="mx-auto max-w-xl lg:mx-0 flex flex-col gap-2">
+        <p class="text-[1.2rem] font-semibold tracking-tighter text-zinc-900">
+          Live rendered view
         </p>
-        <.island id="1" type={:react} component="ReactCounter" data={%{"counter" => @counter}}>
-          <div class="w-full flex flex-row gap-3 items-center justify-between">
-            <span>Server State: <%= @counter %></span>
-            <button
-              class="phx-submit-loading:opacity-75 rounded-lg bg-zinc-900 hover:bg-zinc-700 py-2 px-3 text-sm font-semibold leading-6 text-white active:text-white/80"
-              phx-click="update_counter"
-              phx-value-counter={@counter - 1}
-            >
-              LiveView -1
-            </button>
-          </div>
-        </.island>
 
-        <div class="hidden" />
-
-        <.island id="2" type={:react} component="ReactSharedCounter">
+        <.island id="2" live_data type={:react} component="ReactSharedCounter">
           <div class="w-full flex flex-row gap-3 items-center justify-between">
             <span>Server State: <%= @counter %></span>
             <button
@@ -38,6 +24,7 @@ defmodule ExampleWeb.IFrameIslandsLive do
         </.island>
         <.island
           id="3"
+          live_data
           type={:data}
           data={%{"counter" => @counter}}
           component="Logger"
